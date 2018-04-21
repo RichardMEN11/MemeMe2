25c18fff5a9ec7773e117edd2cc68861ebff7575
Richard <info@richardmenning.de>
2018-04-21 11:31:35 +0200

    Initial Commit

---
+++ MemeMe2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe2
+//
+//  Created by Richard Menning on 21.04.18.
+//  Copyright © 2018 Richard Menning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
