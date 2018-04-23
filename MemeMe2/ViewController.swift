b4150d84231721a14c417302331a5299426aec65
Richard <info@richardmenning.de>
2018-04-23 14:43:39 +0200

    started the project again

--- MemeMe2/ViewController.swift
+++ MemeMe2/ViewController.swift
@@ -7,12 +7,45 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
+    @IBOutlet weak var memeViewController: UIImageView!
+    @IBOutlet weak var pickButton: UIBarButtonItem!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    let imageViewController = UIImagePickerController()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        topTextField.delegate = self
+        bottomTextField.delegate = self
+        topTextField.textAlignment = .center
+        topTextField.text = "TOP"
+        bottomTextField.textAlignment = .center
+        bottomTextField.text = "BOTTOM"
+  
+        let memeTextAttributes:[String: Any] = [
+            NSAttributedStringKey.strokeColor.rawValue: UIColor.black,
+            NSAttributedStringKey.foregroundColor.rawValue: UIColor.white,
+            NSAttributedStringKey.font.rawValue: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSAttributedStringKey.strokeWidth.rawValue: 1]
+        
+        topTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +53,45 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pickButtonPressed(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) == false {
+            return print("photo library not available")
+        }
+        imageViewController.delegate = self
+        imageViewController.sourceType = .photoLibrary
+        self.present(imageViewController, animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraButtonPressed(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) == false {
+            return print("camera not available")
+        }
+        imageViewController.delegate = self
+        imageViewController.sourceType = .camera
+        self.present(imageViewController, animated: true, completion: nil)
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        memeViewController.image = chosenImage
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.text = ""
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+       textField.resignFirstResponder()
+        return false
+    }
+    
+    
 
 }
 
